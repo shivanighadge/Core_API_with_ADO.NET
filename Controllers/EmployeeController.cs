8e6426a7475efaaf0f7514b53ed4ea4e2c2e0601
Shivani ghadge <shivanighadge1@gmail.com>
2025-11-19 21:43:52 +0530

    Added controller

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using API_CRUD_ADO.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_CRUD_ADO.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeDBContext context;
+
+
+        public EmployeeController(EmployeeDBContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetEmployees()
+        {
+             var employees = context.GetEmployees().ToList();
+            return Ok(employees);
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(Employee emp)
+        {
+            var result = context.AddEmployees(emp);
+            return Ok(result);
+        }
+    }
+}
